b833b6159220c209a2a60d20ff64e56b5cc9981c
Jeahoon Jung <jhoon5245@gmail.com>
2020-12-29 10:37:14 +1300

    3. createAccount.dto.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,24 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsEmail, IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
 type UserRole = 'client' | 'owner' | 'delivery';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
   @Column()
+  @Field(() => String)
+  @IsEmail()
   email: string;
 
   @Column()
+  @Field(() => String)
   password: string;
 
   @Column()
+  @Field(() => String)
   role: UserRole;
 }
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,20 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dts';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => Boolean)
+  hi() {
+    return true;
+  }
+
+  @Mutation(() => CreateAccountOutput)
+  createAccount(@Args('input') createAccountInput: CreateAccountInput) {}
+}
