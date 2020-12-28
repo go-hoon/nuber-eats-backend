09b8575a036cca4cb307c0c625518845c1f20b26
Jeahoon Jung <jhoon5245@gmail.com>
2020-12-29 07:42:34 +1300

    #3.6 Optional Types and Columns

--- src/example/entities/example.entity.ts
+++ src/example/entities/example.entity.ts
@@ -1,4 +1,5 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
@@ -10,10 +11,13 @@ export class Example {
 
   @Field(() => String) // 이 field는 String을 반환한다
   @Column() // TypeORM Entity를 위한 Column
+  @Length(5)
   name: string;
 
-  @Field(() => Boolean, { nullable: true })
-  @Column()
+  @Field(() => Boolean, { defaultValue: true }) // GraphQL을 위한 defaultValue 설정
+  @Column({ default: true }) // TypeORM을 위한 defaultValue 설정
+  @IsOptional() // validation을 위한 것 (값이 주어지지 않으면 나머지 validation을 무시한다.)
+  @IsBoolean()
   isGood?: boolean; // nullable이기 때문에 옆에 ?를 붙인다.
 
   @Field(() => String)
