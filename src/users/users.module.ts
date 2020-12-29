fe92d148f67c9c858e054f0e646627a9aba21e30
Jeahoon Jung <jhoon5245@gmail.com>
2020-12-29 17:28:03 +1300

    #5.4 JWT Module part Two

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from 'src/jwt/jwt.module';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
