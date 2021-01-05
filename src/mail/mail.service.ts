b159d87c2fdfff4a9eb0a2502b1a89005b4dd192
Jeahoon Jung <jhoon5245@gmail.com>
2021-01-05 19:07:27 +1300

    #6.7 Mailgun API

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+import got from 'got';
+import * as FormData from 'form-data';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {
+    console.log(options);
+    this.sendEmail('hello', 'how are you?');
+  }
+
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `jhoon5245@gmail.com`);
+    form.append('text', content);
+    form.append('subject', subject);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+        method: 'POST',
+      },
+    );
+    console.log(response.body);
+  }
+}
