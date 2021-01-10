621bb1114a4e69c85411ee0283a2cb11941f04e3
Jeahoon Jung <jhoon5245@gmail.com>
2021-01-10 23:50:19 +1300

    #10.15 Pagination

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -156,21 +156,34 @@ export class RestaurantsService {
     return this.restaurants.count({ category });
   }
 
-  async findCategoryBySlug({ slug }: CategoryInput): Promise<CategoryOutput> {
+  async findCategoryBySlug({
+    slug,
+    page,
+  }: CategoryInput): Promise<CategoryOutput> {
     try {
-      const category = await this.categories.findOne(
-        { slug },
-        { relations: ['restaurants'] },
-      );
+      const category = await this.categories.findOne({ slug });
       if (!category) {
         return {
           ok: false,
           error: 'Category no found',
         };
       }
+
+      const restaurants = await this.restaurants.find({
+        where: {
+          category,
+        },
+        take: 25,
+        skip: (page - 1) * 25,
+      });
+
+      category.restaurants = restaurants;
+      const totalPages = (await this.countRestaurants(category)) / 25;
+
       return {
         ok: true,
         category,
+        totalPages: Math.ceil(totalPages),
       };
     } catch (e) {
       return {
